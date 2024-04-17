2ea74561df08841a28f3c602ac348e7751f9f3e7
Otavio Zabaleta <otaviokz@gmail.com>
2024-04-16 21:32:57 -0300

    First commit

---
+++ TVMazeAssessmentVIPER/TVMazeAssessmentVIPERApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeAssessmentVIPERApp.swift
+//  TVMazeAssessmentVIPER
+//
+//  Created by Otávio Zabaleta on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeAssessmentVIPERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
